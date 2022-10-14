15e850a847f99335a34edbe5431552052a0115e4
jheart-vic <adebowalevictorjheart@gmail.com>
2022-10-14 23:05:08 +0100

    add permit for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :user_params
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :current_password) }
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
   end
 end
