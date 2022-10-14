ae17914d0c4f6bf9fc0a6de851620eecd967af6b
jheart-vic <adebowalevictorjheart@gmail.com>
2022-10-14 21:27:45 +0100

    Add changes to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,13 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-
+  
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
-  config.action_mailer.default_url_options = {:host => 'https://budgetory.herokuapp.com/', :port => 3000}
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host:  'https://budgetory.herokuapp.com/', port:  3000 }
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -61,8 +63,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Budget_App_production"
-
-  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
