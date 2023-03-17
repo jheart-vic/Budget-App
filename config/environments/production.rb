f3f551d77c2e24bd85ee5de3f9a78a3b738329cb
jheart-vic <adebowalevictorjheart@gmail.com>
2023-03-17 17:11:18 +0100

    Add master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,6 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
-  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.default_url_options = { host:  'https://budgetory.herokuapp.com/' }
